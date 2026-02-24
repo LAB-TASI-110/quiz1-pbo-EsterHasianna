5849e6d3e5813e72b265b95ccc910eab5915bfdc
EsterHasianna <esternainggolan780@gmail.com>
2026-02-24 13:22:02 +0700

    Update Driver2.java

--- src/driver/Driver2.java
+++ src/driver/Driver2.java
@@ -1,3 +1,39 @@
+import java.util.Scanner;
+
 public class Driver2 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Input jumlah data
+        int N = input.nextInt();
+
+        // Input nilai
+        int[] nilai = new int[N];
+        for (int i = 0; i < N; i++) {
+            nilai[i] = input.nextInt();
+        }
+
+        // Input kode kelompok
+        int kelompok = input.nextInt();
+
+        int index = 0;
+        int currentKelompok = 1;
+        int total = 0;
+
+        while (index < N) {
+            if (currentKelompok == kelompok) {
+                for (int i = 0; i < currentKelompok && index < N; i++) {
+                    total += nilai[index];
+                    index++;
+                }
+                break;
+            } else {
+                index += currentKelompok;
+            }
+            currentKelompok++;
+        }
+
+        System.out.println(total);
+    }
 }
+
